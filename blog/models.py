e28cd13c00ed0ee5d520a5fea74575e797f78b6a
bashooff <bashooff@hotmail.com>
2021-05-31 14:26:56 +0200

    table is working
    html table is working. have to manually insert an id column in db browser

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,9 @@ from django.db import models
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
     customer = models.TextField(null=True)
-    orderedItemID = models.IntegerField()
-    amount = models.IntegerField()
-    gender = models.TextField()
+    orderedItemID = models.IntegerField(null=True)
+    amount = models.IntegerField(null=True)
+    gender = models.TextField(null=True)
 
     def __str__(self):
         return self.customer
